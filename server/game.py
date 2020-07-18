b60dc4e5b24597b6a1d9b6dc3d1b5aa925190aff
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-07-18 18:10:16 -0400

    cleaned up method to create games

--- server/game.py
+++ server/game.py
@@ -1,20 +1,43 @@
 from flask import Blueprint, request
 from server.serverMethods import *
+import chess
+import random
 
 game = Blueprint("game", __name__)
 
-approvedPlayers = set()
+approvedPlayers = {}
+setupReady = False
 
 
+# maybe seperate method calls in a switch statement and call them from some type of master method depending on the header
 @game.route("/game", methods=["GET", "POST"])
-def beginGame():
-    checkSession()
-    print(session)
-    if session['userID'] not in approvedPlayers and len(approvedPlayers) < 2:
-        approvedPlayers.add(session['userID'])
-    print(session)
-    print(approvedPlayers)
-    if len(approvedPlayers) < 2:
-        return "waiting for player"
-    else:
+def createGame():
+    def gameReady():
+        setupReady = True
         return "ready"
+    approvedUser = lambda : session['userID'] in approvedPlayers
+    global setupReady
+    lobbySize = len(approvedPlayers)
+    checkSession()
+
+    if not setupReady and lobbySize < 2:
+        print("here")
+        print(lobbySize)
+        if not approvedUser:
+            approvedPlayers[session['userID']] = None
+            print(approvedPlayers)
+            return "waiting" if len(approvedPlayers) < 2 else gameReady()
+        elif approvedUser:
+            return "waiting"
+        else:
+            gameReady()
+    if approvedUser:
+        return "ready"
+    elif not approvedUser:
+        #todo. 404 error
+        return "unauthorized"
+
+
+def setupGame():
+    board = chess.Board()
+
